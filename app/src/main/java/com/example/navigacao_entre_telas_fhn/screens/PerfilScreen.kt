9a1091e84aad5a937c9a93f29450be84b18e08c0
labsfiapFabioNardella <fabiohnardella@gmail.com>
2026-03-24 20:24:43 -0300

    Texto na página de perfil centralizado

--- app/src/main/java/com/example/navigacao_entre_telas_fhn/screens/PerfilScreen.kt
+++ app/src/main/java/com/example/navigacao_entre_telas_fhn/screens/PerfilScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -39,7 +41,8 @@ fun PerfilScreen(
                 text = "Perfil de - ${nome} que tem ${idade} anos",
                 fontSize = 30.sp,
                 fontWeight = FontWeight.Bold,
-                color = Color.Red
+                color = Color.Red,
+                style = TextStyle(textAlign = TextAlign.Center)
             )
             Button(
                 onClick = { navController.navigate("menu") },
